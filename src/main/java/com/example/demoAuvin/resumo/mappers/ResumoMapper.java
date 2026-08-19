b1eea6ee4a1c0da00dd66fa227c1c61df95dc5d3
matheus-auv <matheus@auvin-technologies.com.br>
2026-08-19 10:14:53 -0300

    remove: removendo dto desnecessário

--- src/main/java/com/example/demoAuvin/resumo/mappers/ResumoMapper.java
+++ src/main/java/com/example/demoAuvin/resumo/mappers/ResumoMapper.java
@@ -1,12 +1,9 @@
 package com.example.demoAuvin.resumo.mappers;
 
-import com.example.demoAuvin.resumo.dto.ResumoFindFavResponse;
 import com.example.demoAuvin.resumo.dto.ResumoListResponse;
 import com.example.demoAuvin.resumo.dto.ResumoSaveResponse;
 import com.example.demoAuvin.resumo.entities.Resumo;
 import org.mapstruct.Mapper;
-
-import java.util.UUID;
 
 @Mapper(componentModel = "spring")
 public interface ResumoMapper {
